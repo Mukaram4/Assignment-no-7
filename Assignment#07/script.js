2c1ceac67611a6911820667a3114b2244877fb53
Mukaram4 <129292460+Mukaram4@users.noreply.github.com>
2023-03-31 17:22:14 +0500

    Add files via upload

---
+++ Assignment#07/script.js
@@ -0,0 +1,78 @@
+// 1:Write a js program to find maximum between two numbers.
+
+// function Number() {
+//     var Number1 =+ prompt("Enter a first number");
+//     var Number2 =+ prompt("Enter a second number");
+
+// if (Number1>Number2) {
+//     console.log('maximum number is:',Number1);
+    
+// }
+// else{
+//     console.log('maximum number is :',Number2 );
+// }
+
+// }
+// Number()
+
+// 2: Write a js program to find a maximum between three numbers.
+
+// function number() {
+
+//     var Number1 =+ prompt("enter your first number:");
+//     var Number2 =+ prompt("enter your Second number:");
+//     var Number3 =+ prompt("enter your third number:");
+
+//     if (Number1>Number2) {
+//         console.log('maximum number is:',Number1);
+        
+//     }
+
+//     else if (Number2>Number3) {
+//     console.log('maximum number is:',Number2);
+//     }
+
+//     else {
+//         console.log('maximum number is:',Number3);
+
+//     }
+
+// }
+// number()
+
+// 3: Write a program to check  whether a number is even or odd.
+
+// function number() {
+//     var Number1 =+ prompt("Enter a number:");
+//     if (Number1%2==0) {
+
+//         console.log('Your number is even:');
+        
+//     }
+//     else{
+//         console.log('Your is odd:');
+//     }
+
+// }
+// number()
+
+// 4: Write a program to check  whether a number is positive , negative or zero.
+
+var Number1 =+ prompt('Enter a number:');
+
+
+if (Number1 > 0 ) {
+    console.log('your number is positive');
+    
+}
+else if (Number1 < 0) {
+    console.log('your number is negative');
+
+}
+
+else {
+
+    console.log('your number is zero');
+
+}
+
